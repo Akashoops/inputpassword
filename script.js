dda6876fbed544a41f58201337c34ec66d615603
Akash km <akashmanoj999@gmail.com>
2023-05-23 11:57:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function validate(){
+let p= document.getElementById("pass").value
+let p1= document.getElementById("pass1").value
+
+if(p==p1){
+    alert("same")
+}else{
+    alert("wrong")
+}
+
+}
